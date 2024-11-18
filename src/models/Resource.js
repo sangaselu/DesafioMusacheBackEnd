f07f3c269b833f1971185c6f53b04d299c46224e
Sandra Gabriela Seminario <sandra.seminario@outlook.com>
2024-11-17 19:58:21 -0500

    Update Resource.js

--- src/models/Resource.js
+++ src/models/Resource.js
@@ -1,29 +1,25 @@
-const { DataTypes } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = require('../utils/database');
 
-const Resource = sequelize.define(
-  'Resource',
-  {
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-    },
-    imageUrl: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    youtubeUrl: {
-      type: DataTypes.STRING,
-      allowNull: true, 
-    },
+const Resource = sequelize.define('Resource', {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
   },
-  {
-    timestamps: false,
-  }
-);
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  youtubeUrl: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+}, {
+  timestamps: true,
+});
 
 module.exports = Resource;
