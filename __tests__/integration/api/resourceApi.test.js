4b62b69fa57a91ef3c91ed328ffd9a33ce1f0a33
Sandra Gabriela Seminario <sandra.seminario@outlook.com>
2024-11-19 14:47:22 -0500

    Update resourceApi.test.js

--- __tests__/integration/api/resourceApi.test.js
+++ __tests__/integration/api/resourceApi.test.js
@@ -1,18 +1,27 @@
 const request = require('supertest');
-const app = require('../../../src/index');
-const sequelize = require('../../../src/utils/database');
-
-beforeAll(async () => {
-  await sequelize.sync({ force: true }); 
-});
-
-afterAll(async () => {
-  await sequelize.close();
-});
+const { app } = require('../../../src/index');
 
 describe('Pruebas de la API de recursos', () => {
+  let server;
+  let baseUrl;
+
+  beforeAll((done) => {
+ 
+    server = app.listen(0, () => {
+      const { port } = server.address();
+      baseUrl = `http://localhost:${port}`; 
+      console.log(`Servidor de pruebas corriendo en ${baseUrl}`);
+      done();
+    });
+  });
+
+  afterAll(() => {
+
+    server.close();
+  });
+
   it('Debe obtener una lista vacía de recursos al inicio', async () => {
-    const response = await request(app).get('/resources');
+    const response = await request(baseUrl).get('/api/resources'); 
     expect(response.status).toBe(200);
     expect(response.body).toEqual([]);
   });
